6ab9dd7c98605a02a187340df903f2a6f358470b
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2024-02-18 13:09:54 -0300

    feat: endpoints listado ordenes por comprador y vendedor

--- backend/carrito/src/main/java/com/verdumarket/carrito/controller/OrdenController.java
+++ backend/carrito/src/main/java/com/verdumarket/carrito/controller/OrdenController.java
@@ -1,7 +1,30 @@
 package com.verdumarket.carrito.controller;
 
+import com.verdumarket.carrito.model.Orden;
+import com.verdumarket.carrito.service.IOrdenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+
 public class OrdenController {
+    @Autowired
+    private IOrdenService ordenService;
+
+    @GetMapping("/listarConsumidor/{idConsumidor}")
+    public List<Orden> listarOrdenesPorConsumidor(@PathVariable Long idConsumidor){
+        List<Orden> ordenes = ordenService.listarOrdenesPorConsumidor(idConsumidor);
+        return ordenes;
+    }
+
+    @GetMapping("/listarVendedor/{idVendedor}")
+    public ResponseEntity<List<Orden>> listarOrdenesPorVendedor(@PathVariable Long idVendedor){
+        List<Orden> ordenes = ordenService.listarOrdenesPorVendedor(idVendedor);
+        return ResponseEntity.ok(ordenes);
+    }
 }
--- backend/carrito/src/main/java/com/verdumarket/carrito/service/OrdenService.java
+++ backend/carrito/src/main/java/com/verdumarket/carrito/service/OrdenService.java
@@ -1,7 +1,24 @@
 package com.verdumarket.carrito.service;
 
+import com.verdumarket.carrito.model.Orden;
+import com.verdumarket.carrito.repository.OrdenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrdenService implements IOrdenService{
+    @Autowired
+    private OrdenRepository ordenRepository;
+
+    public List<Orden> listarOrdenesPorConsumidor(Long idConsumidor){
+       // return ordenRepository.listarOrdenesPorConsumidor(idConsumidor);
+       return ordenRepository.findAllByIdComprador(idConsumidor);
+    }
+
+    public List<Orden> listarOrdenesPorVendedor(Long idVendedor){
+        //return ordenRepository.listarOrdenesPorVendedor(idVendedor);
+        return ordenRepository.findAllByIdVendedor(idVendedor);
+    }
 }
