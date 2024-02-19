426b1c93e2efcba2a9d4de792aea63eff057b90b
Alexandro Moroz <AlexandroMoroz5@gmail.com>
2024-02-18 23:31:09 -0300

    feat: :sparkles: created 4 classes and 1 SQL script.
    User entity and SQL script are created and coded, user controller, repository and service are empty.

---
+++ backend/usuario-service/src/main/java/com/verdumarket/usuarioservice/UsuarioEntity.java
@@ -0,0 +1,35 @@
+package com.verdumarket.usuarioservice;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "usuario")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String email;
+
+    private String contrasenia;
+
+    private String direccion;
+
+    private String celular;
+
+    private boolean esAdmin;
+
+    private int estado;
+
+}
---
+++ backend/usuario-service/src/main/java/com/verdumarket/usuarioservice/UsuarioService.java
@@ -0,0 +1,8 @@
+package com.verdumarket.usuarioservice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+}
