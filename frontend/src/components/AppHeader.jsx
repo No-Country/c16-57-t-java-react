fb19ae0d5feaf706f08acf89d4a2bf4b47ea9d8c
Matias Baldanza <matiasbaldanza@gmail.com>
2024-02-19 21:58:27 -0300

    feat: AppHeader - cambiar color segun Figma y reemplazar AppMenu (sin uso) por AppLogo transparente

--- frontend/src/components/AppHeader.jsx
+++ frontend/src/components/AppHeader.jsx
@@ -1,11 +1,15 @@
-import IconMenu from "@icons/IconMenu";
+import { useLocation } from "react-router-dom";
+import AppLogo from "@components/AppLogo";
 
 const AppHeader = () => {
+  const location = useLocation();
+  const title = location.pathname.split("/")[2]
+
   return (
-    <header className="flex justify-between h-12 p-4 align-top text-md bg-accent text-primary-content">
-      <h1>Nombre del Usuario</h1>
+    <header className="flex justify-between h-12 p-4 text-lg font-bold capitalize align-top bg-transparent text-md text-accent">
+      <h1>{title}</h1>
       <nav>
-        <IconMenu />
+        <AppLogo variant="transparent" size="small"/>
       </nav>
     </header>
   );
