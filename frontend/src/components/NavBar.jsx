1510385caf3eec3b3946a83e244973aaaaff9102
Matias Baldanza <matiasbaldanza@gmail.com>
2024-02-19 22:18:06 -0300

    fix: cambiar clase .active a .nav-active en NavBar (agregada por NavLink de react-router-dom) por compatibilidad con estilos de DaisyUI (btm-nav). Ajustar colores según Figma última versión

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -11,21 +11,26 @@ const NavBar = () => {
   /* tomar el modo ("vendedor" | "comprador") del primer param de location después de "/" */
   const modo = location.pathname.split("/")[1];
 
+  /* Definir clase .active que agrega NavLink a .nav-active por compatilibidad con DaisyUI */
+  const navLinkClasses = ({ isActive }) => [
+    isActive ? "nav-active" : "",
+  ].join(" ");
+
   return (
     <nav className="p-0 text-xs text-center h-14 rounded-t-xl btm-nav bg-primary/25 text-accent">
-      <NavLink to={`${modo}/home`}>
+      <NavLink to={`${modo}/home`} className={navLinkClasses}>
         <IconHome />
         <span className="sr-only btm-nav-label">Home</span>
       </NavLink>
-      <NavLink to={`${modo}/search`}>
+      <NavLink to={`${modo}/search`} className={navLinkClasses}>
         <IconSearch />
         <span className="sr-only btm-nav-label">Buscar</span>
       </NavLink>
-      <NavLink to={`${modo}/history`}>
+      <NavLink to={`${modo}/history`} className={navLinkClasses}>
         <IconHistory/>
         <span className="sr-only btm-nav-label">Historial</span>
       </NavLink>
-      <NavLink to={`${modo}/profile`}>
+      <NavLink to={`${modo}/profile`} className={navLinkClasses}>
         <IconProfile />
         <span className="sr-only btm-nav-label">Perfil</span>
       </NavLink>
