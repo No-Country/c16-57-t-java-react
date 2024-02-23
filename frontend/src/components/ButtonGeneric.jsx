ff13a9cae69d48e5f06a3db73c1cf21ad42debee
FerJuan <fbatres@gmail.com>
2024-02-23 15:27:18 -0300

    añadidas imagenes para agregar a botones

--- frontend/src/components/ButtonGeneric.jsx
+++ frontend/src/components/ButtonGeneric.jsx
@@ -16,25 +16,26 @@ const ButtonGeneric = ({
     img
 }) => {
   const variantClasses = {
-    'contained': 'bg-primary text-primary-content',
+    'contained': 'bg-white text-accent hover:bg-primary hover:text-white transition-colors duration-300 ease-in-out',
     'outlined': 'border-2 border-primary',
     'transparent': 'bg-transparent',
   }; 
 
   const sizeClasses = {
     'small': 'h-8 p-1 text-sm',
-    'medium': 'size-22 p-2 text-base',
-    'large': 'size-25 p-2 text-xl',
+    'medium': 'size-25 p-2 text-base',
+    'large': 'h-15 p-4 text-xl',
+    'landscape': 'h-[56px] text-base w-full flex flex-row '
   };
 
   const baseClasses = "rounded-xl aspect-square w-full"
 
   return (
-    <div className="flex flex-col w-full gap-2 font-custom">
-      <Link to={to} className="bottom-2">
+    <div className="flex flex-col w-full  font-custom drop-shadow-2xl" >
+      <Link to={to} className="">
         <button className={twMerge(baseClasses, variantClasses[variant], sizeClasses[size], className)}>
          {img && <img src={img} alt={`Imagen de productos ${img} de Verdumarket`} />
- }         {text}
+ }         <p>{text}</p>
         </button>
       </Link>
     </div>
--- frontend/src/views/comprador/CompradorHome.jsx
+++ frontend/src/views/comprador/CompradorHome.jsx
@@ -1,10 +1,27 @@
 import ButtonGeneric from '@components/ButtonGeneric'
 import { SearchBar } from '@components/SearchBar'
-const CompradorHome = () => {
+import naranjas from '../../assets/frutas/naranjas.png'
+import zucchini from '../../assets/verduras/zucchini.png'
+import nueces from '../../assets/frutos_secos/nueces.png'
+import cilantro from '../../assets/aromaticas/cilantro.png'
+import productores from '../../assets/productores.png'
+const CompradorHome = ({user= "usuario"}) => {
   return ( 
-    <div className="flex flex-col gap-4 items-center justify-center bg-red-100 w-full h-full">
+
+
+<div className="flex flex-col gap-4 items-center justify-center w-full h-full">
+    <section className='text-accent'>
+      <h1 className="text-3xl font-bold">¡Bienvenido {user}!</h1>
+      <p className="text-xl">¿Qué te gustaría comprar hoy?</p>
+    </section>
    <SearchBar className="items-start"/>
-    <ButtonGeneric text="Boton Generico" variant="outlined"/>   
+   <section className='grid grid-cols-2 gap-4'>
+    <ButtonGeneric text="Frutas" size="large"  img={naranjas}/>   
+    <ButtonGeneric text="Verduras" size="large"  img={zucchini}/>   
+    <ButtonGeneric text="Aromaticas" size="large"  img={cilantro}/>   
+    <ButtonGeneric text="Frutos Secos"size="large"  img={nueces}/>   
+   </section>
+    <ButtonGeneric text="Productores" size="landscape"  img={productores}/>   
  </div>
   )
 }
