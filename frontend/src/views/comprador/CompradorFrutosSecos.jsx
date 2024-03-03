c8cde46fae6bf96e635b97c52549ec5ddf774b5d
Ferjuan <fbatres@gmail.com>
2024-03-03 17:13:27 -0300

    hello

--- frontend/src/views/comprador/CompradorFrutosSecos.jsx
+++ frontend/src/views/comprador/CompradorFrutosSecos.jsx
@@ -1,4 +1,6 @@
-
+import SearchForm from "../../components/SearchForm";
+import ButtonGeneric from "../../components/ButtonGeneric";
+import nueces from '../../assets/frutos_secos/nueces.png'
 const CompradorFrutosSecos = () => {
     const frutosSecos = [
       {
@@ -35,8 +37,26 @@ const CompradorFrutosSecos = () => {
       
     ]
     return (
-      <div>CompradorFrutosSecos</div>
+      <>
+      <section className="h-2/8  sticky top-0 text-center w-full mb-4 z-20 bg-[#F6F6F6] rounded-lg">
+          <h1 className='text-3xl font-custom font-medium text-accent mb-2'>Frutas</h1>
+          <SearchForm/>
+      </section>
+      <section className="flex flex-col h-6/8 gap-3 ">
+  {frutosSecos.map((fruta,index)=>{
+    return(
+      <div className="">
+  
+      <ButtonGeneric key={index} text={fruta.nombre} text2={fruta.Unidad?"por unidad":"por kilo"} img={nueces} imgClassName="rounded-full size-14" size="landscape" className="" to={`/comprador/${fruta.nombre}`}/>
+      
+      </div>
     )
+  })}
+  
+      </section>
+      </>
+    );
+    
   }
   
   export default CompradorFrutosSecos
