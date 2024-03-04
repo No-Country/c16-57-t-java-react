39cfec0b36ff4cae8024a01aa617ade9e1e185ef
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2024-03-04 18:25:41 -0300

    fix: cors

---
+++ backend/eureka/src/main/java/com/verdumarket/eureka/configuration/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.verdumarket.eureka.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://verdumarket.vercel.app", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
