7bfb9a2268cc2e51cf031ac186177265b6d44516
Ferjuan <fbatres@gmail.com>
2024-03-05 09:00:07 -0300

    agregado servicio de guardar productos en BDD

---
+++ frontend/src/services/datosBDD.js
@@ -0,0 +1,348 @@
+const productos = [
+    {
+      "nombreProducto": "Manzana",
+      "categoria": {
+        "idCategoria": 2
+      },
+      "Unidad": "Kg",
+      "descripcion": "Manzanas jugosas y dulces, ideales para un snack saludable o como ingrediente en tus postres favoritos."
+    },
+    {
+      "nombreProducto": "Banana",
+      "categoria": {
+        "idCategoria": 2
+      },
+      "Unidad": "Unidad",
+      "descripcion": "Bananas maduras y energéticas, perfectas para un desayuno rápido o una merienda nutritiva."
+    },
+    {
+      "nombreProducto": "Uva",
+      "categoria": {
+        "idCategoria": 2
+      },
+      "Unidad": "Unidad",
+      "descripcion": "Uvas suculentas y dulces, ideales para disfrutar como snack o para acompañar tus tablas de quesos y embutidos."
+    },
+    {
+      "nombreProducto": "Pera",
+      "categoria": {
+        "idCategoria": 2
+      },
+      "Unidad": "Kg",
+      "descripcion": "Peras jugosas y aromáticas, perfectas para consumir frescas o en la preparación de deliciosos postres y tartas."
+    },
+    {
+      "nombreProducto": "Naranja",
+      "categoria": {
+        "idCategoria": 2
+      },
+      "Unidad": "Unidad",
+      "descripcion": "Naranjas frescas y jugosas, ricas en vitamina C y perfectas para disfrutar en zumo, ensaladas o como snack saludable."
+    },
+    {
+      "nombreProducto": "Kiwi",
+      "categoria": {
+        "idCategoria": 2
+      },
+      "Unidad": "Unidad",
+      "descripcion": "Kiwis exóticos y refrescantes, con un sabor único y una textura suave, ideales para añadir a tus smoothies y postres."
+    },
+    {
+      "nombreProducto": "Mango",
+      "categoria": {
+        "idCategoria": 2
+      },
+      "Unidad": "Unidad",
+      "descripcion": "Mangos dulces y jugosos, perfectos para disfrutar en ensaladas, postres o como snack tropical y nutritivo."
+    },
+    {
+      "nombreProducto": "Fresa",
+      "categoria": {
+        "idCategoria": 2
+      },
+      "Unidad": "Kg",
+      "descripcion": "Fresas rojas y dulces, ideales para consumir frescas, en macedonia, tartas o como ingrediente en tus batidos y smoothies."
+    },
+    {
+      "nombreProducto": "Melón",
+      "categoria": {
+        "idCategoria": 2
+      },
+      "Unidad": "Unidad",
+      "descripcion": "Melones jugosos y refrescantes, perfectos para disfrutar en verano como snack saludable o en la preparación de deliciosas ensaladas."
+    },
+    {
+      "nombreProducto": "Sandía",
+      "categoria": {
+        "idCategoria": 2
+      },
+      "Unidad": "Unidad",
+      "descripcion": "Sandías dulces y jugosas, ideales para refrescarse en los días más calurosos y disfrutar de un snack bajo en calorías."
+    },
+    {
+      "nombreProducto": "Cilantro",
+      "categoria": {
+        "idCategoria": 5
+      },
+      "Unidad": "Unidad",
+      "descripcion": "Cilantro fresco y aromático, perfecto para dar un toque especial a tus platos de cocina internacional, como la comida mexicana o asiática."
+    },
+    {
+      "nombreProducto": "Albahaca",
+      "categoria": {
+        "idCategoria": 5
+      },
+  
+"Unidad": "Unidad",
+"descripcion": "Albahaca fresca y aromática, ideal para acompañar tus platos de pasta, ensaladas y pizza, aportando un sabor único e intenso."
+},
+{
+"nombreProducto": "Menta",
+"categoria": {
+  "idCategoria": 5
+},
+"Unidad": "Unidad",
+"descripcion": "Menta fresca y aromática, perfecta para preparar tus infusiones, cócteles y postres, aportando un toque de frescor y sabor inigualable."
+},
+{
+"nombreProducto": "Aromaticas",
+"categoria": {
+  "idCategoria": 5
+},
+"Unidad": "Kg",
+"descripcion": "Surte de hierbas aromáticas frescas y seleccionadas, ideales para dar un toque especial a tus platos y recetas favoritas."
+},
+{
+"nombreProducto": "Tomate",
+"categoria": {
+  "idCategoria": 1
+},
+"Unidad": "Unidad",
+"descripcion": "Tomates maduros y jugosos, perfectos para ensaladas, salsas y cualquier preparación que requiera un toque de sabor fresco y natural."
+},
+{
+"nombreProducto": "Lechuga",
+"categoria": {
+  "idCategoria": 1
+},
+"Unidad": "Unidad",
+"descripcion": "Lechuga fresca y crujiente, ideal para ensaladas, sándwiches y wraps, aportando un toque de frescor y ligereza a tus comidas."
+},
+{
+"nombreProducto": "Espinaca",
+"categoria": {
+  "idCategoria": 1
+},
+"Unidad": "Unidad",
+"descripcion": "Espinaca fresca y nutritiva, perfecta para ensaladas, cremas y guisos, aportando un toque de sabor y color a tus platos."
+},
+{
+"nombreProducto": "Perejil",
+"categoria": {
+  "idCategoria": 1
+},
+"Unidad": "Unidad",
+"descripcion": "Perejil fresco y aromático, ideal para dar un toque de sabor y color a tus platos, como guisos, sopas, ensaladas y aderezos."
+},
+{
+"nombreProducto": "Verduras de hoja",
+"categoria": {
+  "idCategoria": 1
+},
+"Unidad": "Kg",
+"descripcion": "Surtido de verduras de hoja frescas y seleccionadas, perfectas para ensaladas, cremas y guisos, aportando un toque de sabor y nutrición a tus comidas."
+},
+{
+"nombreProducto": "Cebolla",
+"categoria": {
+  "idCategoria": 1
+},
+"Unidad": "Unidad",
+"descripcion": "Cebollas frescas y aromáticas, ideales para dar un toque de sabor a tus platos, como guisos, ensaladas, sofritos y aderezos."
+},
+{
+"nombreProducto": "Zanahoria",
+"categoria": {
+  "idCategoria": 1
+},
+"Unidad": "Unidad",
+"descripcion": "Zanahorias frescas y crujientes, perfectas para consumir crudas, cocidas o en jugo, aportando un toque de sabor dulce y colorido a tus comidas."
+},
+{
+"nombreProducto": "Remolacha",
+"categoria": {
+  "idCategoria": 1
+},
+"Unidad": "Unidad",
+"descripcion": "Remolachas frescas y nutritivas, ideales para ensaladas, jugos y guisos, aportando un toque de sabor y color intenso a tus platos."
+},
+{
+"nombreProducto": "Rucula",
+"categoria": {
+  "idCategoria": 1
+},
+"Unidad": "Unidad",
+"descripcion": "Rúcula fresca y aromática, perfecta para ensaladas, sándwiches y pizzas, aportando un toque de sabor picante y distintivo a tus comidas."
+},
+  {
+    "nombreProducto": "Apio",
+    "categoria": {
+      "idCategoria": 1
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Apio fresco y crujiente, ideal para ensaladas, cremas y guisos, aportando un toque de sabor y textura únicos a tus platos."
+  },
+  {
+    "nombreProducto": "Puerro",
+    "categoria": {
+      "idCategoria": 1
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Puerros frescos y aromáticos, perfectos para dar un toque de sabor a tus platos, como guisos, sopas, ensaladas y aderezos."
+  },
+  {
+    "nombreProducto": "Calabaza",
+    "categoria": {
+      "idCategoria": 1
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Calabaza fresca y dulce, ideal para cremas, guisos y postres, aportando un toque de sabor y color a tus comidas."
+  },
+  {
+    "nombreProducto": "Batata",
+    "categoria": {
+      "idCategoria": 1
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Batatas dulces y nutritivas, perfectas para asar, hornear o freír, aportando un toque de sabor y textura únicos a tus platos."
+  },
+  {
+    "nombreProducto": "Avellana",
+    "categoria": {
+      "idCategoria": 3
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Avellanas crujientes y sabrosas, ideales para consumir como snack, en la preparación de postres o para acompañar tus ensaladas favoritas."
+  },
+  {
+    "nombreProducto": "Almendra",
+    "categoria": {
+      "idCategoria": 3
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Almendras crujientes y nutritivas, perfectas para consumir como snack, en la preparación de postres o para acompañar tus ensaladas favoritas."
+  },
+  {
+    "nombreProducto": "Nuez",
+    "categoria": {
+      "idCategoria": 3
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Nueces sabrosas y nutritivas, ideales para consumir como snack, en la preparación de postres o para acompañar tus ensaladas favoritas."
+  },
+  {
+    "nombreProducto": "Castaña",
+    "categoria": {
+      "idCategoria": 3
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Castañas dulces y aromáticas, perfectas para asar en el horno o consumir como snack, aportando un toque de sabor y textura únicos a tus comidas."
+  },
+  {
+    "nombreProducto": "Pimienta negra",
+    "categoria": {
+      "idCategoria": 5
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Pimienta negra en grano, ideal para moler y dar un toque picante y aromático a tus platos favoritos."
+  },
+  {
+    "nombreProducto": "Canela en rama",
+    "categoria": {
+      "idCategoria": 5
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Canela en rama, perfecta para dar un toque dulce y aromático a tus postres, infusiones y guisos."
+  },
+  {
+    "nombreProducto": "Clavo de olor",
+    "categoria": {
+      "idCategoria": 5
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Clavo de olor, una especia aromática y versátil, ideal para dar un toque especial a tus platos dulces y salados."
+  },
+  {
+    "nombreProducto": "Orégano",
+    "categoria": {
+      "idCategoria": 5
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Orégano seco, perfecto para dar un toque mediterráneo y aromático a tus platos de pasta, pizza y carnes."
+  },
+  {
+    "nombreProducto": "Romero",
+    "categoria": {
+      "idCategoria": 5
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Romero fresco y aromático, ideal para dar un toque especial a tus platos de carne, pescado y patatas."
+  },
+  {
+    "nombreProducto": "Tomillo",
+    "categoria": {
+      "idCategoria": 5
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Tomillo seco, perfecto para dar un toque mediterráneo y aromático a tus guisos, asados y sopas."
+  },
+  {
+    "nombreProducto": "Cúrcuma",
+    "categoria": {
+      "idCategoria": 5
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Cúrcuma en polvo, una especia aromática y colorida, ideal para dar un toque exótico y saludable a tus platos."
+  },
+  {
+    "nombreProducto": "Jengibre",
+    "categoria": {
+      "idCategoria": 5
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Jengibre fresco, perfecto para dar un toque picante y aromático a tus platos asiáticos, infusiones y postres."
+  },
+  {
+    "nombreProducto": "Comino",
+    "categoria": {
+      "idCategoria": 5
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Comino en grano, ideal para dar un toque aromático y ligeramente picante a tus guisos, asados y panes."
+  },
+  {
+    "nombreProducto": "Pimentón",
+    "categoria": {
+      "idCategoria": 5
+    },
+    "Unidad": "Unidad",
+    "descripcion": "Pimentón dulce o picante, perfecto para dar un toque de color y sabor a tus platos de carne, pescado y verduras."
+  }
+  
+]
+const URLProductos = 'http://localhost:443/PRODUCTO-SERVICE/nuevoProductosGen'
+
+const requests = productos.map(producto => {
+    return fetch(URLProductos, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(producto)
+    });
+  });
+  
+  Promise.all(requests)
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
